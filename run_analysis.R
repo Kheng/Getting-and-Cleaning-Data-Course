5bacde7ecbdf6cf9a466355af23790d550e87105
Kheng <Kheng@users.noreply.github.com>
2014-10-26 10:58:34 +0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -14,7 +14,7 @@ X<-rbind(XTrain, XTest)
 Train <- read.table("C:/Users/admin/Documents/UCI HAR Dataset/train/subject_train.txt")
 Test <- read.table("C:/Users/admin/Documents/UCI HAR Dataset/test/subject_test.txt")
 subject<-rbind(Train, Test)
-names(subject)<-"Volunteer"
+names(subject)<-"Activity_ID"
 features <- read.table("C:/Users/admin/Documents/UCI HAR Dataset/features.txt")
 names(X)<-features[,2]
 
@@ -27,8 +27,9 @@ tidy <- cbind(subject, Y, X)
 write.table(tidy, "TidyDatav1.txt")
 
 ## Calculating the Average of Each variable##
-agg_mean <-aggregate(tidy[,-2], by=list(tidy$Activity,tidy$Volunteer),FUN=mean, na.rm=TRUE)
+agg_mean <-aggregate(tidy[,-2], by=list(tidy$Activity,tidy$Activity_ID),FUN=mean, na.rm=TRUE)
 agg_mean<-agg_mean[-2]
 agg_mean<-agg_mean[c(2,1,3:68)]
 names(agg_mean)[names(agg_mean)=="Group.1"] <- "Activity"
 write.table(agg_mean, "TidyDatav2.txt")
+
